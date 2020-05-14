707f42798f6e42ab9dbeb7db5e53f2606fd190c1
Ridana <dariutja@gmail.com>
2020-05-14 20:28:10 +0300

    Добавьте файлы проекта.

---
+++ basedata_01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace basedata_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(UserContext db = new UserContext())
+            {
+                var users = db.Users;
+                Console.WriteLine("Список людей");
+                foreach(User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+            Console.Read();
+        }
+    }
+    public class UserContext : DbContext
+    {
+        public UserContext() :
+                base("UserDB")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+    
+    public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+
+
